5c1f63161afbbbb7f4a01a385b5dc589c247ed24
Avinash Vijayvargiya <66490629+avinash14022002@users.noreply.github.com>
2022-10-04 02:51:55 +0530

    [XML] activity_registration.xml (#7)

--- app/src/main/java/com/twofa/duosec/registration/RegistrationActivity.kt
+++ app/src/main/java/com/twofa/duosec/registration/RegistrationActivity.kt
@@ -1,17 +1,92 @@
 package com.twofa.duosec.registration
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
-import com.twofa.duosec.databinding.ActivityRegistrationBinding
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.budiyev.android.codescanner.CodeScanner
+import com.budiyev.android.codescanner.DecodeCallback
+import com.budiyev.android.codescanner.ErrorCallback
+import com.twofa.duosec.R
+import com.twofa.duosec.recoverycode.RecoveryCodeActivity
 
 class RegistrationActivity : AppCompatActivity() {
-
-    private lateinit var binding: ActivityRegistrationBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityRegistrationBinding.inflate(layoutInflater)
-        val view = binding.root
-        setContentView(view)
+        setContentView(R.layout.activity_registration)
+
+        hintView = findViewById(R.id.scanner_hint)
+        codeScanner = CodeScanner(this, findViewById(R.id.scanner))
+
+        codeScanner.decodeCallback = DecodeCallback {
+            runOnUiThread {
+//                hintView?.text = result.text
+                if (hintView?.text != null)
+                    startActivity(Intent(this, RecoveryCodeActivity::class.java))
+            }
+        }
+        codeScanner.errorCallback = ErrorCallback { error: Throwable ->
+            runOnUiThread {
+                Toast.makeText(this, getString(R.string.scanner_error, error), Toast.LENGTH_LONG)
+                    .show()
+            }
+        }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                isPermissionGranted = false
+                requestPermissions(
+                    arrayOf(Manifest.permission.CAMERA),
+                    RC_PERMISSION
+                )
+            } else {
+                isPermissionGranted = true
+            }
+        } else {
+            isPermissionGranted = true
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(
+            requestCode,
+            permissions,
+            grantResults
+        )
+        if (requestCode == RC_PERMISSION) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                isPermissionGranted = true
+                codeScanner.startPreview()
+            } else {
+                isPermissionGranted = false
+            }
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if (isPermissionGranted) {
+            codeScanner.startPreview()
+        }
+    }
+
+    override fun onPause() {
+        codeScanner.releaseResources()
+        super.onPause()
+    }
+
+    private lateinit var codeScanner: CodeScanner
+    private var hintView: TextView? = null
+    private var isPermissionGranted: Boolean = false
+
+    companion object {
+        private const val RC_PERMISSION = 10
     }
 }
