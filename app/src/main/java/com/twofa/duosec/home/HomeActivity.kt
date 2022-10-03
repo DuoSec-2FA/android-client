6af47f961ef5fcaaeded47732a2c239e27ff438e
Avinash Vijayvargiya <66490629+avinash14022002@users.noreply.github.com>
2022-10-04 01:47:57 +0530

    [XML] activity_home.xml (#6)

--- app/src/main/java/com/twofa/duosec/home/HomeActivity.kt
+++ app/src/main/java/com/twofa/duosec/home/HomeActivity.kt
@@ -1,8 +1,15 @@
 package com.twofa.duosec.home
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import com.twofa.duosec.R
 import com.twofa.duosec.databinding.ActivityHomeBinding
+import com.twofa.duosec.fingerprint.FingerPrintActivity
+import com.twofa.duosec.registration.RegistrationActivity
+
 
 class HomeActivity : AppCompatActivity() {
 
@@ -14,5 +21,28 @@ class HomeActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
         supportActionBar?.title = "Duo Sec"
+
+        binding.fabAddCompany.setOnClickListener {
+            startActivity(Intent(this, RegistrationActivity::class.java))
+        }
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.home, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        val id: Int = item.itemId
+        if (id == R.id.action_item_one) {
+            startActivity(Intent(this, FingerPrintActivity::class.java))
+            return true
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
